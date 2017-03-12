276de04c77cbf13bf2fb6043dfef4e0d443fd614
Horev Ivan <ivanhorev@icons8.com>
2017-03-13 00:52:55 +0700

    added empty Sample project

---
+++ Icons8.UI.Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Icons8.UI.Sample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(I8_Assembly.Company)]
+[assembly: AssemblyProduct(I8_Assembly.Product)]
+[assembly: AssemblyCopyright(I8_Assembly.Copyright)]
+[assembly: AssemblyTrademark(I8_Assembly.Trademark)]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion(I8_Assembly.BuildVersion)]
+[assembly: AssemblyFileVersion(I8_Assembly.MajorVersion)]
+
+[assembly: ComVisible(false)]
