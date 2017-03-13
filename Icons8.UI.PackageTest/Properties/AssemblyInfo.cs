bd596eff0a0d28fb9fc053b2308337f3291b4186
Horev Ivan <ivanhorev@icons8.com>
2017-03-13 12:05:47 +0700

    add project for package testing

---
+++ Icons8.UI.PackageTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Icons8.UI.PackageTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Icons8 LLC")]
+[assembly: AssemblyProduct("Icons8.UI.PackageTest")]
+[assembly: AssemblyCopyright("Copyright © Icons8 2017")]
+[assembly: AssemblyTrademark("Icons8")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
